78c722cf13fff8aa65d736656da2c1bd8308b424
Will Rogers <wahrogers@gmail.com>
2021-05-06 09:46:51 +0100

    Additions to bash prompt.

--- prompt.sh
+++ prompt.sh
@@ -1,9 +1,15 @@
-# Complex PS1
+# Complex PS1.
+# Possible additions:
+# * distinguish between changed and staged files.
+
 # \001 and \002 are better than \[ and \] when used inside a function.
+# https://stackoverflow.com/a/43462720/980151
 RED="\001$(tput setaf 1)\002"
 YELLOW="\001$(tput setaf 3)\002"
 BLUE="\001$(tput setaf 4)\002"
+PALEBLUE="\001$(tput setaf 6)\002"
 GREEN="\001$(tput setaf 10)\002"
+PALEYELLOW="\001$(tput setaf 11)\002"
 BOLD="\001$(tput bold)\002"
 RESET="\001$(tput sgr0)\002"
 
@@ -28,4 +34,9 @@ function dollar() {
 }
 # Using PROMPT_COMMAND prevents other tools such as virtualenv from modifying
 # the prompt. So use PS1 directly.
-PS1="$GREEN\u$BLUE\W$YELLOW\$(gitstatus)$RESET\$(dollar) "
+# Changing colour at the end changes the colour of typed text.
+PS1="$GREEN\u$PALEBLUE\W$YELLOW\$(gitstatus)\$(dollar) $PALEYELLOW"
+
+# Reset any colours after command executed.
+# https://unix.stackexchange.com/questions/198723/change-color-of-input-text-not-output
+trap 'echo -ne "\e[0m"' DEBUG
