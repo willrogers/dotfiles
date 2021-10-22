a469272ad5d420dab792a2e226e6e23b00566cc6
Will Rogers <william.rogers@oup.com>
2021-10-22 09:50:02 +0100

    More useful config.fish.

--- config.fish
+++ config.fish
@@ -1,4 +1,26 @@
+# It can help to know when these files are sourced.
+echo "sourcing config.fish" >&2
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-fish_add_path /opt/homebrew/opt/ruby/bin
+
+function fish_prompt
+    set_color green
+    echo -n $USER
+    set_color blue
+    echo -n (basename (pwd))
+    set branch (git branch --show-current 2> /dev/null || echo -n "")
+    set_color cyan
+    echo -n $branch
+    set_color yellow
+    echo -n "\$ "
+    set_color normal
+end
+
+# Some aliases
+alias gittree "git log --graph --oneline"
+alias gf "git fetch --all --prune"
+alias gp "git pull --ff-only"
+alias gdiff "git diff --no-index"
+alias gs "git status"
