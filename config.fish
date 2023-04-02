dbbed90ece6e995672da9957d46f17fb2cd339f9
Will Rogers <will.rogers@oup.com>
2023-04-02 10:18:33 +0100

    Updates to fish config.

--- config.fish
+++ config.fish
@@ -1,18 +1,36 @@
 # It can help to know when these files are sourced.
-echo "sourcing config.fish" >&2
+echo "Sourcing config.fish ..." >&2
+
+set DEFAULT_USER rogersw
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
 function fish_prompt
-    set_color green
-    echo -n $USER
+    set last_status $status
+    if not test $USER = "$DEFAULT_USER"
+        set_color red
+        echo -n $USER
+    end
     set_color blue
     echo -n (basename (pwd))
     set branch (git branch --show-current 2> /dev/null || echo -n "")
     set_color cyan
     echo -n $branch
+    set gitstatus (git status 2> /dev/null)
+    if string match -q "*Changes not staged*" "$gitstatus"
+        set_color brgreen
+        echo -n "*"
+    end
+    if string match -q "*Changes to be committed*" "$gitstatus"
+        set_color brgreen
+        echo -n "+"
+    end
+    if not test $last_status = 0
+        set_color red
+        echo -n "[$last_status]"
+    end
     set_color yellow
     echo -n "\$ "
     set_color normal
